dd766b7acd28a93fde98561c08686c318bf659eb
지준삼 <jsji@daou.co.kr>
2022-10-31 16:43:49 +0900

    release용 커밋

---
+++ docs/js/about.154e72e5.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkvue_lol_gg"]=self["webpackChunkvue_lol_gg"]||[]).push([[443],{2526:function(a,e,n){n.r(e),n.d(e,{default:function(){return v}});var u=n(3396);const t=a=>((0,u.dD)("data-v-6d31ce1a"),a=a(),(0,u.Cn)(),a),c={class:"about"},s=t((()=>(0,u._)("h1",null,"This is an about page",-1))),r=[s];function d(a,e){return(0,u.wg)(),(0,u.iD)("div",c,r)}var l=n(89);const o={},i=(0,l.Z)(o,[["render",d],["__scopeId","data-v-6d31ce1a"]]);var v=i}}]);
+//# sourceMappingURL=about.154e72e5.js.map
