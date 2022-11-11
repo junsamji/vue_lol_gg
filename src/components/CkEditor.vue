d43d05b81751c1b23f33682d5144a3c9e5325c0d
지준삼 <jsji@daou.co.kr>
2022-11-11 15:38:06 +0900

    ckEditor 추가 / 반응형 css적용

---
+++ src/components/CkEditor.vue
@@ -0,0 +1,30 @@
+<template>
+  <textarea id="editor1" name="editor1"></textarea>
+</template>
+<script>
+import { onMounted, ref, watch } from 'vue'
+export default {
+  name: 'CkEditor',
+  props: ['content'],
+  setup(props, { emit }) {
+    const content = ref(props.content)
+    let ckEditor
+    onMounted(() => {
+      // initalize ckeditor
+      ckEditor = window.CKEDITOR.replace('editor1')
+      // setting Data
+      ckEditor.setData(content.value)
+      // on change event
+      ckEditor.on('change', () => {
+        emit('sendContent', ckEditor.getData())
+      })
+    })
+    watch(content, () => {
+      if (content.value !== ckEditor.getData()) {
+        ckEditor.setData(content.value)
+      }
+    })
+  }
+}
+</script>
+<style></style>
---
+++ src/views/EditorView.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="editor">
+    <h1>Editor 연습</h1>
+    <div id="app">
+      <CkEditor @sendContent="getContent" :content="intialData"></CkEditor>
+    </div>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+import CkEditor from '@/components/CkEditor.vue'
+export default {
+  name: 'EditorView',
+  components: { CkEditor },
+  setup() {
+    const intialData = ref('hello world')
+    const getContent = (val) => {
+      console.log(val)
+    }
+    return { getContent, intialData }
+  }
+}
+</script>
+
+<style scoped>
+.editor {
+  width: 100%;
+  margin-top: 100px;
+}
+</style>
